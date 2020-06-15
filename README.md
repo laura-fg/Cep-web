a965cb1818fc06ca06310bc7c6e02534739c5c61
laura-fg <lauura_1998_@hotmail.com>
2020-06-15 20:48:19 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cep-web
+  A seguinte aplicação propõe fornecer informações de localização de acordo com o CEP  informado.
+  
---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+function consultacep(){
+
+   var cep = document.getElementById("cep").value;
+   var url = "https://viacep.com.br/ws/" + cep + "/json/";
+   console.log(url);
+
+   $.ajax({
+       url: url,
+       type: "GET",
+       success: function(response){
+           console.log(response);
+           $("#logradouro").html(response.logradouro);
+
+           $("#bairro").html(response.bairro);
+
+           $("#localidade").html( response.localidade);
+
+           $("#uf").html(response.uf);
+
+           $("#titulo").html("Consulta do CEP:  " + response.cep);
+       }
+   })
+}
